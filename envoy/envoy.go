67ad9b99b9c7fefd90af3b2fca98fea5d700df79
daltonhahn <hahnd11@gmail.com>
2021-03-06 16:45:03 -0600

    Separating envoy logic

---
+++ envoy/envoy.go
@@ -0,0 +1,165 @@
+package envoy
+
+import (
+	"fmt"
+	"io/ioutil"
+	"gopkg.in/yaml.v2"
+	"os"
+	"log"
+	"strconv"
+)
+
+const listener_config = `resources:
+- "@type": type.googleapis.com/envoy.config.listener.v3.Listener
+  name: listener_0
+  address:
+    socket_address:
+      address: 0.0.0.0
+      port_value: 80
+  filter_chains:
+  - filters:
+      name: envoy.filters.network.http_connection_manager
+      typed_config:
+        "@type": type.googleapis.com/envoy.extensions.filters.network.http_connection_manager.v3.HttpConnectionManager
+        stat_prefix: ingress_http
+        http_filters:
+        - name: envoy.filters.http.router
+        route_config:
+          name: local_route
+          virtual_hosts:
+          - name: backend
+            domains:
+            - "*"
+            routes:
+            - match:
+                prefix: "/anvil"
+              route:
+                prefix_rewrite: "/"
+                cluster: anvil_service
+`
+
+
+type Service struct {
+	Name	string
+	Port	int64
+}
+
+type EnvoyConfig struct {
+	Services	[]Service
+}
+
+func writeAnvilCluster(f_cds *os.File) *os.File {
+	if _, err := f_cds.WriteString(`resources:
+- "@type": type.googleapis.com/envoy.config.cluster.v3.Cluster
+  name: anvil_service
+  connect_timeout: 30s
+  type: LOGICAL_DNS
+  dns_lookup_family: V4_ONLY
+  load_assignment:
+    cluster_name: anvil_service
+    endpoints:
+    - lb_endpoints:
+      - endpoint:
+          address:
+            socket_address:
+              address: 0.0.0.0
+              port_value: 8080
+
+`); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	return f_cds
+}
+
+func writeCDS(svc string, port int64, f_cds *os.File) *os.File {
+	if _, err := f_cds.WriteString("- \"@type\": type.googleapis.com/envoy.config.cluster.v3.Cluster\n"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_cds.WriteString("  name: " + svc + "_service\n"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_cds.WriteString(`  connect_timeout: 30s
+  type: LOGICAL_DNS
+  dns_lookup_family: V4_ONLY
+  load_assignment:
+  `); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_cds.WriteString("  cluster_name: " + svc + "_service"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_cds.WriteString(`
+    endpoints:
+    - lb_endpoints:
+      - endpoint:
+          address:
+            socket_address:
+              address: 0.0.0.0
+`); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_cds.WriteString("              port_value: " + strconv.FormatInt(port,10) + "\n\n"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	return f_cds
+}
+
+func writeLDS(svc string, port int64, f_lds *os.File) *os.File {
+	if _, err := f_lds.WriteString("            - match:\n"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_lds.WriteString("                prefix: \"/service/" + svc + "\"\n"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_lds.WriteString("              route:\n"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	if _, err := f_lds.WriteString("                cluster: " + svc + "_service\n"); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+
+	return f_lds
+}
+
+func SetupEnvoy() {
+	f_lds, err := os.OpenFile("/root/anvil/envoy/config/lds.yaml", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		log.Printf("Open file error %v", err)
+	}
+	defer f_lds.Close()
+
+	f_cds, err := os.OpenFile("/root/anvil/envoy/config/cds.yaml", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	if err != nil {
+		log.Printf("Open file error %v", err)
+	}
+	defer f_cds.Close()
+
+	f_cds = writeAnvilCluster(f_cds)
+	c, err := readEnvoyConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := f_lds.WriteString(listener_config); err != nil {
+		log.Printf("Writing error %v", err)
+	}
+	for _,ele := range c.Services {
+		fmt.Printf("%s:%d\n", ele.Name, ele.Port)
+		f_cds = writeCDS(ele.Name, ele.Port, f_cds)
+		f_lds = writeLDS(ele.Name, ele.Port, f_lds)
+	}
+}
+
+func readEnvoyConfig() (*EnvoyConfig, error) {
+	yamlFile, err := ioutil.ReadFile("/root/anvil/envoy/config/sample-svc.yaml")
+	if err != nil {
+		log.Printf("Read file error #%v", err)
+	}
+	c := &EnvoyConfig{}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	return c, nil
+}
