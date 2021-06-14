2914100a85892db1b6f9fef7746bc6198bba7c45
daltonhahn <hahnd11@gmail.com>
2021-06-14 17:14:53 -0500

    Making iptables clean better

--- network/clean-tables.sh
+++ network/clean-tables.sh
@@ -4,6 +4,7 @@ iptables -t nat -F PREROUTING
 iptables -t nat -F PROXY_INIT_REDIRECT
 iptables -t nat -F PROXY_INIT_REDIRECT_OUTBOUND
 iptables -t nat -F PROXY_INIT_OUTPUT
+iptables -t nat -D OUTPUT 3 
 iptables -t nat -D OUTPUT 2
 
 iptables -t nat --delete-chain PROXY_INIT_REDIRECT
