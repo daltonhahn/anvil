ff1f62e530db1aea8ecaca50a633f224565c6e3a
daltonhahn <hahnd11@gmail.com>
2021-06-16 11:52:53 -0500

    This one compiles

--- rotation/rotation.go
+++ rotation/rotation.go
@@ -52,15 +52,15 @@ func CollectFiles(iter string, nodeName string) bool {
 		fmt.Printf("FAILURE WRITING OUT FILE CONTENTS\n")
 	}
 
-	fMess := &FPMess{FilePath: nodeName+"/acl.yaml"}
-	jsonData, err := json.Marshal(fMess)
+	fMess = &FPMess{FilePath: nodeName+"/acl.yaml"}
+	jsonData, err = json.Marshal(fMess)
 	if err != nil {
 		log.Fatalln("Unable to marshal JSON")
 	}
-	postVal := bytes.NewBuffer(jsonData)
-	resp, err := security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
+	postVal = bytes.NewBuffer(jsonData)
+	resp, err = security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
 
-	out, err := os.Create("/root/anvil/config/acls/"+iter+"/acl.yaml")
+	out, err = os.Create("/root/anvil/config/acls/"+iter+"/acl.yaml")
 	if err != nil  {
 		fmt.Printf("FAILURE OPENING FILE\n")
 	}
@@ -74,15 +74,15 @@ func CollectFiles(iter string, nodeName string) bool {
 		fmt.Printf("FAILURE WRITING OUT FILE CONTENTS\n")
 	}
 
-	fMess := &FPMess{FilePath: nodeName+"/"+nodeName+".key"}
-	jsonData, err := json.Marshal(fMess)
+	fMess = &FPMess{FilePath: nodeName+"/"+nodeName+".key"}
+	jsonData, err = json.Marshal(fMess)
 	if err != nil {
 		log.Fatalln("Unable to marshal JSON")
 	}
-	postVal := bytes.NewBuffer(jsonData)
-	resp, err := security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
+	postVal = bytes.NewBuffer(jsonData)
+	resp, err = security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
 
-	out, err := os.Create("/root/anvil/config/certs/"+iter+"/"+nodeName+".key")
+	out, err = os.Create("/root/anvil/config/certs/"+iter+"/"+nodeName+".key")
 	if err != nil  {
 		fmt.Printf("FAILURE OPENING FILE\n")
 	}
@@ -96,15 +96,15 @@ func CollectFiles(iter string, nodeName string) bool {
 		fmt.Printf("FAILURE WRITING OUT FILE CONTENTS\n")
 	}
 
-	fMess := &FPMess{FilePath: nodeName+"/"+nodeName+".crt"}
-	jsonData, err := json.Marshal(fMess)
+	fMess = &FPMess{FilePath: nodeName+"/"+nodeName+".crt"}
+	jsonData, err = json.Marshal(fMess)
 	if err != nil {
 		log.Fatalln("Unable to marshal JSON")
 	}
-	postVal := bytes.NewBuffer(jsonData)
-	resp, err := security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
+	postVal = bytes.NewBuffer(jsonData)
+	resp, err = security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
 
-	out, err := os.Create("/root/anvil/config/certs/"+iter+"/"+nodeName+".crt")
+	out, err = os.Create("/root/anvil/config/certs/"+iter+"/"+nodeName+".crt")
 	if err != nil  {
 		fmt.Printf("FAILURE OPENING FILE\n")
 	}
@@ -120,15 +120,15 @@ func CollectFiles(iter string, nodeName string) bool {
 
 
 	/*
-	fMess := &FPMess{FilePath: nodeName+"/ca.crt"}
-	jsonData, err := json.Marshal(fMess)
+	fMess = &FPMess{FilePath: nodeName+"/ca.crt"}
+	jsonData, err = json.Marshal(fMess)
 	if err != nil {
 		log.Fatalln("Unable to marshal JSON")
 	}
-	postVal := bytes.NewBuffer(jsonData)
-	resp, err := security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
+	postVal = bytes.NewBuffer(jsonData)
+	resp, err = security.TLSPostReq(qMem, "/service/rotation/bundle"+iter, "rotation", "application/json", postVal)
 
-	out, err := os.Create("/root/anvil/config/certs/"+iter+"/ca.crt")
+	out, err = os.Create("/root/anvil/config/certs/"+iter+"/ca.crt")
 	if err != nil  {
 		fmt.Printf("FAILURE OPENING FILE\n")
 	}
@@ -151,21 +151,3 @@ func CollectFiles(iter string, nodeName string) bool {
 		// nodeName.key
 	return true
 }
-
-
-
-/*
-out, err := os.Create("/root/anvil-rotation/artifacts/"+pullMap.Iteration+"/"+f)
-                                        if err != nil  {
-                                                fmt.Printf("FAILURE OPENING FILE\n")
-                                        }
-                                        defer out.Close()
-                                        defer resp.Body.Close()
-                                        if resp.StatusCode != http.StatusOK {
-                                                fmt.Errorf("bad status: %s", resp.Status)
-                                        }
-                                        _, err = io.Copy(out, resp.Body)
-                                        if err != nil  {
-                                                fmt.Printf("FAILURE WRITING OUT FILE CONTENTS\n")
-                                        }
-					*/
