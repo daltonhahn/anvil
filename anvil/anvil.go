167b2281b892c96ffef139d9da73232bae20751c
daltonhahn <hahnd11@gmail.com>
2021-07-04 00:49:20 -0500

    Moving server start back to where it was

--- anvil/anvil.go
+++ anvil/anvil.go
@@ -88,7 +88,6 @@ func AnvilInit(nodeType string) {
                         <-sigHandle
 			if rotFlag == true {
 				rotFlag = false
-				go cw.startNewServer(anv_router)
 				ctxShutDown, cancel := context.WithTimeout(context.Background(), (2*time.Second))
 				defer func() {
 					cancel()
@@ -97,6 +96,7 @@ func AnvilInit(nodeType string) {
 				if err := server.Shutdown(ctxShutDown); err != nil {
 					log.Fatalf("server Shutdown Failed:%+s", err)
 				}
+				go cw.startNewServer(anv_router)
 			}
                 }
                 wg.Done()
