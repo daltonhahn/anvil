88c4e827295048401598d7c06e20691ce41925a2
daltonhahn <hahnd11@gmail.com>
2021-07-05 22:26:54 -0500

    Back to 10 sec gossip

--- anvil/gossip/gossip.go
+++ anvil/gossip/gossip.go
@@ -143,7 +143,7 @@ func CheckHealth() {
 				}
 			}
 		}
-		time.Sleep(3 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 }
 
@@ -198,6 +198,6 @@ func PropagateCatalog() {
 			}
 			sendCatalogSync(receivedStuff.Nodes[target].Name, jsonData)
 		}
-		time.Sleep(3 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 }
