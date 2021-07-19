60a474de6777c3d51ae546ab33afd338d2ae95d7
daltonhahn <hahnd11@gmail.com>
2021-07-19 09:58:19 -0500

    Making really small udp buffer

--- anvil/anvil.go
+++ anvil/anvil.go
@@ -111,7 +111,7 @@ func AnvilInit(nodeType string) {
 }
 
 func registerUDP() {
-	p := make([]byte, 64*1024)
+	p := make([]byte, 64)
 	addr := net.UDPAddr{
 		Port: 443,
 		IP: net.ParseIP("0.0.0.0"),
