1d9a54c3d2ee5b5566d264f4b044fc9589801ac9
daltonhahn <hahnd11@gmail.com>
2021-12-13 14:18:15 -0600

    Hopefully easy fix

--- router/router.go
+++ router/router.go
@@ -256,7 +256,7 @@ func TokenLookup(w http.ResponseWriter, r *http.Request) {
         }
 	fmt.Printf("Token Lookup data: %v\n", string(b))
 	var lookupDat []raft.LookupMap
-	err = json.Unmarshal(b, &lookupDat)
+	err = json.Unmarshal(string(b), &lookupDat)
 	fmt.Printf("Object: %v\n", lookupDat)
 	fmt.Printf("After marshalling tok lookup data: %v\n", err)
 	result := raft.TokenLookup(lookupDat, time.Now())
