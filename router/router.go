0fde26255d47999cc8abaeac1ab3fb2356d2e77d
daltonhahn <hahnd11@gmail.com>
2021-12-13 14:29:42 -0600

    String manipulation

--- router/router.go
+++ router/router.go
@@ -257,6 +257,7 @@ func TokenLookup(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Token Lookup data: %v\n", string(b))
 	var lookupDat []raft.LookupMap
 	stripped := strings.Replace(string(b), "\\", "", -1)
+	fmt.Printf("Stripped string: %v\n", stripped)
 	err = json.Unmarshal([]byte(stripped), &lookupDat)
 	fmt.Printf("Object: %v\n", lookupDat)
 	fmt.Printf("After marshalling tok lookup data: %v\n", err)
