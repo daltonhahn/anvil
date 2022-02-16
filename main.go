1cec4b98614e2e09832f2d8e6c625e76d8d3d874
daltonhahn <hahnd11@gmail.com>
2022-02-16 17:33:56 -0500

    Starting to add logging functionality, going to merge with main

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"github.com/daltonhahn/anvil/cli"
+	"github.com/daltonhahn/anvil/logging"
 )
 
 func main() {
+	logging.InitLog()
+	logging.InfoLogger.Println("Starting the Anvil binary. . .")
 	cli.CLI()
 }
